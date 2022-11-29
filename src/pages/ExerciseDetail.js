2f0930a723be3dfb241bb9d0349e9e35d0b83eb0
Andrew <andrewsabatyuk17@gmail.com>
2022-11-29 21:18:02 +0200

    add exercise card component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import Details from '../components/Details'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+import { exerciseOptions, fetchData } from '../utils/fetchDatd'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const {id} = useParams()
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+      
+    }
+    fetchExercisesData()
+  }, [id])
+  
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Details exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
 export default ExerciseDetail
+
+// 1/46
